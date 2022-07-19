1c8ae9fa7d92924285e49dc980f040a7cdf92bed
COOPMEGO\lmorocho <lmorocho@mego.net>
2022-07-19 11:09:18 -0500

    AddTransient por AddSingleton

--- src/Infrastructure/ConfigureInfrastructure.cs
+++ src/Infrastructure/ConfigureInfrastructure.cs
@@ -14,21 +14,21 @@ public static class ConfigureInfrastructure
     public static IServiceCollection AddInfrastructureServices ( this IServiceCollection services )
     {
         //INFRAESTRUCTURA
-        services.AddTransient<ParametersInMemory>( );
-        services.AddTransient<ILogs, LogsService>( );
-        services.AddTransient<IMongoDat, LogsMongoDat>( );
-        services.AddTransient<IDailyRequest, DailyRequest>( );
-        services.AddTransient<IParametersInMemory, ParametersInMemory>( );
-        services.AddTransient<IParametrosDat, ParametrosDat>( );
-        services.AddTransient<IHttpService, HttpService>( );
-        services.AddTransient<IAutenticarseDat, AutenticarseDat>( );
-        services.AddTransient<IEncryptMego, EncryptMego>( );
-        services.AddTransient<IWsOtp, WsOtp>( );
-        services.AddTransient<IOtpDat, OtpDat>( );
+        services.AddSingleton<ParametersInMemory>( );
+        services.AddSingleton<ILogs, LogsService>( );
+        services.AddSingleton<IMongoDat, LogsMongoDat>( );
+        services.AddSingleton<IDailyRequest, DailyRequest>( );
+        services.AddSingleton<IParametersInMemory, ParametersInMemory>( );
+        services.AddSingleton<IParametrosDat, ParametrosDat>( );
+        services.AddSingleton<IHttpService, HttpService>( );
+        services.AddSingleton<IAutenticarseDat, AutenticarseDat>( );
+        services.AddSingleton<IEncryptMego, EncryptMego>( );
+        services.AddSingleton<IWsOtp, WsOtp>( );
+        services.AddSingleton<IOtpDat, OtpDat>( );
 
         //CASOS DE USO
-        services.AddTransient<IEncryptMego, EncryptMego>( );
-        services.AddTransient<IAccesoDat, RecuperarContraseniaDat>( );
+        services.AddSingleton<IEncryptMego, EncryptMego>( );
+        services.AddSingleton<IAccesoDat, RecuperarContraseniaDat>( );
 
 
         return services;
