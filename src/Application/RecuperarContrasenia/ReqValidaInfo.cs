46c3afeabfcf0324bdf5839d12e644dab0b75141
COOPMEGO\lmorocho <lmorocho@mego.net>
2022-09-12 15:20:59 -0500

    desencriptar con llaver rsa

--- src/Application/RecuperarContrasenia/ReqValidaInfo.cs
+++ src/Application/RecuperarContrasenia/ReqValidaInfo.cs
@@ -1,6 +1,7 @@
 ﻿
 using MediatR;
 using Application.Common.ISO20022.Models;
+using static Application.Common.Cryptography.CryptographyRSA;
 
 namespace Application.Acceso.RecuperarContrasenia;
 
@@ -8,5 +9,9 @@ namespace Application.Acceso.RecuperarContrasenia;
 public class ReqValidaInfo : Header, IRequest<ResValidaInfo>
 {
     public string str_num_documento { get; set; } = String.Empty;
+    public void DecryptRSA ( DatosLlaveRsa Key )
+    {
+        str_login = Decrypt(str_login!, Key.str_xml_priv!);
+    }
 }
 
