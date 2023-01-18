cd6b0cf5d7c2b4fa46d01f9483cdc82b7f21ef45
COOPMEGO\lmorocho <lmorocho@mego.net>
2023-01-18 12:39:19 -0500

    parametros iniciales

--- src/Domain/Entities/DatosLlaveRespuesta.cs
+++ src/Domain/Entities/DatosLlaveRespuesta.cs
@@ -10,6 +10,8 @@ namespace Domain.Entities
     {
         public string? str_mod { get; set; }
         public string? str_exp { get; set; }
+        public int int_t_otp { get; set; }
+        public int int_t_inac { get; set; }
 
     }
 }
