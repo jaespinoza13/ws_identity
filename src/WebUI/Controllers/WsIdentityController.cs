6746de290766b12687c4e2d38b9dd6dc82fb3b27
jepullaguari <jepullaguari@mego.net>
2024-01-09 14:44:27 -0500

    fis: habilitar auth en controladores

--- src/WebUI/Controllers/WsIdentityController.cs
+++ src/WebUI/Controllers/WsIdentityController.cs
@@ -12,17 +12,17 @@ namespace WebUI.Controllers
 {
     [Route("api/wsIdentity")]
     [ApiController]
-    //[ServiceFilter(typeof(RequestControl))]
-    //[ProducesResponseType(StatusCodes.Status200OK)]
-    //[ProducesResponseType(StatusCodes.Status400BadRequest)]
-    //[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ServiceFilter(typeof(RequestControl))]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public class WsIdentityController : ApiControllerBase
     {
 
         [HttpPost("autenticarse")]
-        //[ServiceFilter(typeof(CryptographyRSAFilter))]
-        //[ServiceFilter(typeof(CryptographyAESFilter))]
+        [ServiceFilter(typeof(CryptographyRSAFilter))]
+        [ServiceFilter(typeof(CryptographyAESFilter))]
         public async Task<ResAutenticarse> LogIn ( ReqAutenticarse reqAutenticarse )
 
         {
